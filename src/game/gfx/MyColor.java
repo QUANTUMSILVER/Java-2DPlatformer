c0908fd6004a0b0381a864471ef73ff162a3e115
QUANTUMSILVER <39178316+QUANTUMSILVER@users.noreply.github.com>
2018-12-08 12:05:20 +1100

    Added Glow

--- src/game/gfx/MyColor.java
+++ src/game/gfx/MyColor.java
@@ -4,4 +4,5 @@ import java.awt.Color;
 
 public class MyColor {
 	public static final Color TRANS_WHITE_10 = new Color(255, 255, 255, 10);
+	public static final Color TRANS_RED_10 = new Color(255, 0, 0, 10);
 }
